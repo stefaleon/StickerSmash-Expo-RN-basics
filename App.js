9abf54c2225876f489db6a20101f93b8ea60c014
stefaleon <stefanidisl@iconplatforms.com>
2024-03-11 12:21:11 +0200

    Divide components into files

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+ import ImageViewer from './components/ImageViewer'; 
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -7,12 +9,13 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -24,9 +27,5 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 58,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
 });
+
